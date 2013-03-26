d89f15a43c412046191903c9558838f15ed55eef
Peelou <peelou@peelou-HP-EliteBook-8540p.(none)>
2013-03-26 14:09:11 +0100

    bonjour

---
+++ lemipc.h
@@ -0,0 +1,10 @@
+/*
+** lemipc.h for LEMIPC in /home/peelou/SVN
+** 
+** Made by gery baudry
+** Login   <baudry_g@epitech.net>
+** 
+** Started on  Tue Mar 26 11:26:38 2013 gery baudry
+** Last update Tue Mar 26 11:26:43 2013 gery baudry
+*/
+
---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+** main.c for lemipc in /home/peelou/SVN
+** 
+** Made by gery baudry
+** Login   <baudry_g@epitech.net>
+** 
+** Started on  Tue Mar 26 11:22:42 2013 gery baudry
+** Last update Tue Mar 26 11:24:05 2013 gery baudry
+*/
+
+int		main(int ac, char **av)
+{
+  if (ac != 3)
+    {
+      printf("Usage : ./lemipc ...\n");
+      return (0);
+    }
+  else
+    {
+      //run
+    }
+  return (0)
+}
